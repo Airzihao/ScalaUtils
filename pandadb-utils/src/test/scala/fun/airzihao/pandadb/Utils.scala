bbebfa7a23d1ff51753f6446c8d4806bbbc8e257
Airzihao <airzihao@163.com>
2020-12-27 11:45:04 +0800

    Add a new timing func.

--- pandadb-utils/src/test/scala/fun/airzihao/pandadb/Utils.scala
+++ pandadb-utils/src/test/scala/fun/airzihao/pandadb/Utils.scala
@@ -8,10 +8,18 @@ package fun.airzihao.pandadb
  */
 object Utils {
   def timing[T](f: => T): T = {
+    val t1 = System.currentTimeMillis()
+    val t = f
+    val t2 = System.currentTimeMillis()
+    println(s"time cost: ${t2 - t1} ms")
+    t
+  }
+
+  def timingByMicroSec[T](f: => T): T = {
     val t1 = System.nanoTime()
     val t = f
     val t2 = System.nanoTime()
-    println(s"time cost: ${t2 - t1} ms")
+    println(s"time cost: ${((t2 - t1)/1000).toInt} us")
     t
   }
 }
