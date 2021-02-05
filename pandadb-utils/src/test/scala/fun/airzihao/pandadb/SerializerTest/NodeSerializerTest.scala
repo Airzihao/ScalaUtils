ef19b152f32cd6b90d0b1c777c2ef960e2b417bb
Airzihao <airzihao@163.com>
2021-02-05 10:15:46 +0800

    Test

--- pandadb-utils/src/test/scala/fun/airzihao/pandadb/SerializerTest/NodeSerializerTest.scala
+++ pandadb-utils/src/test/scala/fun/airzihao/pandadb/SerializerTest/NodeSerializerTest.scala
@@ -1,9 +1,9 @@
 package fun.airzihao.pandadb.SerializerTest
 
 
-import cn.pandadb.kernel.util.serializer.NodeSerializer
+import cn.pandadb.kernel.util.serializer.{ChillSerializer, NodeSerializer}
 import org.junit.{Assert, Test}
-import fun.airzihao.pandadb.Utils.timing
+import fun.airzihao.pandadb.Utils.{timing, timingByUSec}
 import fun.airzihao.pandadb.kernel.store.StoredNodeWithProperty
 
 /**
@@ -13,7 +13,9 @@ import fun.airzihao.pandadb.kernel.store.StoredNodeWithProperty
  * @Modified By:
  */
 class NodeSerializerTest {
-  val nodeValue = new StoredNodeWithProperty(123456, Array(1), Map(1->1, 2->"dasdadadasdadbb", 3->true))
+  val propMap: Map[Int, Any] = Map(1-> "1988-06-15", 2-> Array("Aline17592188666832@earthling.net", "Aline17592188666832@gmail.com", "Aline17592188666832@yahoo.com"),
+    3->"Aline", 4->"Oliveira", 5->"male", 6->Array("pt", "en"), 7->"Internet Explorer", 8->"198.17.121.2", 9->717592188666832L, 10->"2011-07-04")
+  val nodeValue = new StoredNodeWithProperty(717592188666832L, Array(1), propMap)
   val nodeSerializer = NodeSerializer
 
   @Test
@@ -25,7 +27,12 @@ class NodeSerializerTest {
   def deSerializePERF(): Unit = {
     println("deserialize")
     val bytesArr = nodeSerializer.serialize(nodeValue)
+    val bytesArr1 = ChillSerializer.serialize(nodeValue)
     timing(for (i<-1 to 10000000) nodeSerializer.deserializeNodeValue(bytesArr))
+    timingByUSec(nodeSerializer.deserializeNodeValue(bytesArr))
+    timingByUSec(ChillSerializer.deserialize(bytesArr1, classOf[StoredNodeWithProperty]))
+    val node = nodeSerializer.deserializeNodeValue(bytesArr)
+    println(node.properties)
 
   }
 
